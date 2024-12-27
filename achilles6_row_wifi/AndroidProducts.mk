19e9004fcd83c95215b399133826a3ab13c4e34b
Raif1 <93464125+Raif1@users.noreply.github.com>
2024-12-27 22:40:49 +0600

    Add files via upload

---
+++ achilles6_row_wifi/AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_achilles6_row_wifi.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_achilles6_row_wifi-user \
+    omni_achilles6_row_wifi-userdebug \
+    omni_achilles6_row_wifi-eng
---
+++ achilles6_row_wifi/README.md
@@ -0,0 +1,10 @@
+# Android device tree for Lenovo Lenovo TB-X606F (achilles6_row_wifi)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
